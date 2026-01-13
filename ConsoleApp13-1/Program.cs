2e74c59756ef7dd9580277792d5a4a8ee294a9ae
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-13 14:42:59 +0900

    a

--- ConsoleApp13-1/Program.cs
+++ ConsoleApp13-1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    enum DayOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+    }
+
+    static void Main(string[] args)
+    {
+        foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            
+            Console.WriteLine(day); 
+    }
+}
+
+    
+    
+    
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
